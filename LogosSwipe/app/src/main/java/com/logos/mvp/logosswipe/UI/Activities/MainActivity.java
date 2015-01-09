3b7edeb2e58605a67a051548e2b54e5d0e08f74f
cmonin <yueshanmonin@gmail.com>
2015-01-09 15:30:42 +0100

    1st activities
    Solutions Choice
    Values Choice
    Values Rank

---
+++ LogosSwipe/app/src/main/java/com/logos/mvp/logosswipe/UI/Activities/MainActivity.java
@@ -0,0 +1,53 @@
+package com.logos.mvp.logosswipe.UI.Activities;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.logos.mvp.logosswipe.R;
+import com.logos.mvp.logosswipe.UI.Fragments.ProblemFragment;
+
+public class MainActivity extends ActionBarActivity implements ProblemFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        ProblemFragment fragment = ProblemFragment.newInstance("", "");
+        fragmentTransaction.add(R.id.container, fragment);
+        fragmentTransaction.commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(String id) {
+
+    }
+}
